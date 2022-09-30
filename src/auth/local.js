971b2c6c781b103519c2de43fab701e0b0648281
Fiamma Muscari <fiammamuscari@gmail.com>
2022-09-29 23:56:55 -0300

    [Fix]: fixing some errors

--- src/auth/local.js
+++ src/auth/local.js
@@ -12,28 +12,41 @@ passport.deserializeUser(async (id, done) => {
 })
 
 passport.use('local-register', new LocalStrategy({
+    
     usernameField: 'username',
     passwordField: 'password',
     passReqToCallback: true
-}, 
+
+    }, 
     async (req, username, password, done) => {
 
-        const thereIsUser = await User.findOne({username: username})
+        const thereIsUser = await User.findOne({ username: username })
 
-        if(thereIsUser){
-            return done(null, false, {message:"Username is already taken."})
+        if (thereIsUser) {
+            return done(null, false, req.flash('registerMessage', 'Username already exists'))
         } else {
             const newUser = new User()
             newUser.username = username
             newUser.password = newUser.generateHash(password)
             await newUser.save()
             done(null, newUser)
-        }
+        }        
+}))
 
-
-        
+passport.use('local-login', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, username, password, done) => {
+    const user = await User.findOne({ username })
+    if (!user) {
+        return done(null, false, req.flash('loginMessage', 'No user found'))
     }
-))
+    if (!user.validatePassword(password)) {
+        return done(null, false, req.flash('loginMessage', 'Wrong password'))
+    }
+    done(null, user)
+}))
 
 
 export default passport
--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -3,50 +3,45 @@ import passport from "passport"
 
 const router = express.Router()
 
-
 router
-.get("/auth/login", (req, res, next) => {
-    res.send("Login page")
-})
-.post("/auth/login", passport.authenticate("local-login", {
-    successRedirect: "/profile",
-    failureRedirect: "/auth/login",
-    failureFlash: true
-}))
+    .get("/auth/login", (req, res, next) => {
+        res.send("Login page")
+    })
+    .post("/auth/login", passport.authenticate("local-login", {
+        successRedirect: "/profile",
+        failureRedirect: "/auth/login",
+        failureFlash: true
+    }))
 
-.get("/auth/register", (req, res, next) => {
-    if(registerMessage) {
-        res.send(registerMessage)
-    } else {
-        res.send("Register page")
-    }
-})
-.post("/auth/register", passport.authenticate("local-register", {
-    successRedirect: "/profile",
-    failureRedirect: "/auth/register",
-    passReqToCallback: true
-}))
+    .get("/auth/register", (req, res, next) => {
+            res.send("Register page")
+    })
+    .post("/auth/register", passport.authenticate("local-register", {
+        successRedirect: "/profile",
+        failureRedirect: "/auth/register",
+        passReqToCallback: true
+    }))
 
-.get("/auth/logout", (req, res, next) => {
-    req.logout()
-    res.redirect("/auth/login")
-})
+    .get("/auth/logout", (req, res, next) => {
+        req.logout()
+        res.redirect("/auth/login")
+    })
 
 router.use((req, res, next) => {
-isLoggedIn(req, res, next)
-next()
+    isLoggedIn(req, res, next)
+    next()
 })
 
 router
-.get("/profile", (req, res, next) => {
-    res.send("Profile page")
-})
+    .get("/profile", (req, res, next) => {
+        res.send("Profile page")
+    })
 
-function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) {
-        return next()
+    function isLoggedIn(req, res, next) {
+        if(req.isAuthenticated()) {
+            return next()
+        }
+        res.redirect("/auth/login")
     }
-    res.redirect("/auth/login")
-}
 
 export default router
